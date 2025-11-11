d0a7d076a45baec004e5a4adfee2d2024d9844a1
ahp002 <aliciap.ap6@gmail.com>
2025-11-10 22:22:41 -0800

    added base for form validation with customized error msgs

--- form-with-js.js
+++ form-with-js.js
@@ -1,1 +1,26 @@
-const allowedChars = /^[A-Za-z]*$/;
+const allowedChars1 = /^[A-Za-z]*$/;
+
+function showError(input, message) {
+    const errOut = document.getElementById(input.id + "-error");
+    errOut.textContent = message;
+    errOut.classList.remove("hidden");
+    input.classList.add("flash");
+    setTimeout(() => {
+        errorOutput.classList.add("hidden");
+        input.classList.remove("flash");
+    }, 2000);
+}
+
+function enforceCharacterRules(event) {
+    if (!allowedChars.test(event.target.value)) {
+        showError(event.target, "Invalid character entered.");
+        event.target.value = event.target.value.replace(/[^A-Z a-z\s]/g, "");
+    }
+}
+
+nameInput.addEventListener("input", enforceCharacterRules);
+
+
+nameInput.addEventListener("invalid", (e) => {
+    showError(nameInput, "Please enter your name.");
+});
